139508237b647d0463a6e90c1815083a289a14f5
LouisAugustine <nzlouis.com@gmail.com>
2021-09-26 16:14:40 +1300

    Changed the initial value of the  data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 
-insert into user values('some.one@email.com', 's0m3s3cr3tp455w0rd', 'Some', 'One');
+insert into user values('10@gmail.com', 'RESEARCH', 'Louis', 'Augustine');
 insert into user values('20@gmail.com', 'RESEARCH', 'Tom', 'Smith');
 insert into user values('30@gmail.com', 'SALES', 'James', 'Hudson');
 insert into user values( '40@gmail.com','OPERATIONS', 'Peter', 'William');
